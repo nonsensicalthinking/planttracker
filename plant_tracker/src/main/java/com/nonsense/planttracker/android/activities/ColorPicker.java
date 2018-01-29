fff52f0830adc1de6f677650c05bf92326d36511
Derek Brooks <nonsensicalthinking@gmail.com>
2018-01-28 21:28:56 -0500

    Changed title on color picker to "Color Picker"

--- plant_tracker/src/main/java/com/nonsense/planttracker/android/activities/ColorPicker.java
+++ plant_tracker/src/main/java/com/nonsense/planttracker/android/activities/ColorPicker.java
@@ -47,6 +47,7 @@ public class ColorPicker extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_picker);
+        setTitle("Color Picker");
 
         Intent intent = getIntent();
 
