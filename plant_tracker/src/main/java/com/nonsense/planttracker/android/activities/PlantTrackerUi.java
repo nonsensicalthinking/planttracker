88c22013b8e66b0753a71b786c8c781aae370453
Derek Brooks <nonsensicalthinking@gmail.com>
2018-02-23 22:47:33 -0500

    The sledge...

--- plant_tracker/src/main/java/com/nonsense/planttracker/android/activities/PlantTrackerUi.java
+++ plant_tracker/src/main/java/com/nonsense/planttracker/android/activities/PlantTrackerUi.java
@@ -470,20 +470,20 @@ public class PlantTrackerUi extends AppCompatActivity
         if (currentPlant.getThumbnail() != null)    {
             String thumbnail = currentPlant.getThumbnail();
             if (thumbnail != null)  {
-                Runnable loadThumb = new Runnable() {
-                    @Override
-                    public void run() {
-                        mPlantImage.setImageURI(Uri.fromFile(new File(currentPlant.getThumbnail())));
-                        mPlantImage.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                launchImageSeriesViewer(currentPlant.getAllImagesForPlant());
-                            }
-                        });
-                    }
-                };
-
-                loadThumb.run();
+//                Runnable loadThumb = new Runnable() {
+//                    @Override
+//                    public void run() {
+//                        mPlantImage.setImageURI(Uri.fromFile(new File(currentPlant.getThumbnail())));
+//                        mPlantImage.setOnClickListener(new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View view) {
+//                                launchImageSeriesViewer(currentPlant.getAllImagesForPlant());
+//                            }
+//                        });
+//                    }
+//                };
+//
+//                loadThumb.run();
             }
         }
         else    {
@@ -595,53 +595,53 @@ public class PlantTrackerUi extends AppCompatActivity
             parentPlantLinearLayout.setVisibility(View.GONE);
         }
 
-        Runnable loadRecords = new Runnable() {
-            @Override
-            public void run() {
-                PlantRecordableTileArrayAdapter plantRecordableAdapter =
-                        new PlantRecordableTileArrayAdapter(getBaseContext(),
-                                R.layout.tile_plant_recordable, currentPlant.getAllGenericRecords(),
-                                tracker.getAllRecordTemplates(), currentPlant);
-
-                recordableEventListView.setAdapter(plantRecordableAdapter);
-                recordableEventListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                        presentRecordableEventSummaryDialog(i);
-                    }
-                });
-
-                recordableEventListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-                    @Override
-                    public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
-                        AlertDialog.Builder builder = new AlertDialog.Builder(PlantTrackerUi.this);
-                        builder.setTitle(R.string.app_name);
-                        builder.setMessage("Are you sure you want to delete this event?");
-                        builder.setIcon(R.drawable.ic_growing_plant);
-                        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                currentPlant.removeGenericRecord(position);
-                                fillIndividualPlantView();
-                                dialog.dismiss();
-                            }
-                        });
-
-                        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                dialog.dismiss();
-                            }
-                        });
-
-                        AlertDialog alert = builder.create();
-                        alert.show();
-
-                        return true;
-                    }
-                });
-            }
-        };
-
-        loadRecords.run();
+//        Runnable loadRecords = new Runnable() {
+//            @Override
+//            public void run() {
+//                PlantRecordableTileArrayAdapter plantRecordableAdapter =
+//                        new PlantRecordableTileArrayAdapter(getBaseContext(),
+//                                R.layout.tile_plant_recordable, currentPlant.getAllGenericRecords(),
+//                                tracker.getAllRecordTemplates(), currentPlant);
+//
+//                recordableEventListView.setAdapter(plantRecordableAdapter);
+//                recordableEventListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                    @Override
+//                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                        presentRecordableEventSummaryDialog(i);
+//                    }
+//                });
+//
+//                recordableEventListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//                    @Override
+//                    public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+//                        AlertDialog.Builder builder = new AlertDialog.Builder(PlantTrackerUi.this);
+//                        builder.setTitle(R.string.app_name);
+//                        builder.setMessage("Are you sure you want to delete this event?");
+//                        builder.setIcon(R.drawable.ic_growing_plant);
+//                        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//                            public void onClick(DialogInterface dialog, int id) {
+//                                currentPlant.removeGenericRecord(position);
+//                                fillIndividualPlantView();
+//                                dialog.dismiss();
+//                            }
+//                        });
+//
+//                        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+//                            public void onClick(DialogInterface dialog, int id) {
+//                                dialog.dismiss();
+//                            }
+//                        });
+//
+//                        AlertDialog alert = builder.create();
+//                        alert.show();
+//
+//                        return true;
+//                    }
+//                });
+//            }
+//        };
+//
+//        loadRecords.run();
     }
 
     private void launchCollectPlantDataIntent(GenericRecord record, boolean showNotes) {
