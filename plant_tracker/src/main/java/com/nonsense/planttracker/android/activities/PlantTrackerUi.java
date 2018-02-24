8d26cb3f6d1abaec0ac17c56410e7b5fbd6c3b34
Derek Brooks <nonsensicalthinking@gmail.com>
2018-02-23 21:57:38 -0500

    New apk with crash report option! (Removed test crash)

--- plant_tracker/src/main/java/com/nonsense/planttracker/android/activities/PlantTrackerUi.java
+++ plant_tracker/src/main/java/com/nonsense/planttracker/android/activities/PlantTrackerUi.java
@@ -693,8 +693,6 @@ public class PlantTrackerUi extends AppCompatActivity
      */
     @Override
     public void onBackPressed() {
-        double foo = 12/0;
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
