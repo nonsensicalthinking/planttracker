6e839b7dab17e7ce503c6c4cfaf2b27ecfad5402
Derek Brooks <nonsensicalthinking@gmail.com>
2018-02-23 21:52:30 -0500

    Added crash reporting.  You can now optionally provide me with a crash report via email. New apk!

---
+++ plant_tracker/src/main/java/com/nonsense/planttracker/PlantTracker.java
@@ -0,0 +1,25 @@
+package com.nonsense.planttracker;
+
+import android.app.Application;
+import android.content.Context;
+
+import org.acra.*;
+import org.acra.annotation.*;
+
+/**
+ * Created by Derek Brooks on 2/23/2018.
+ */
+
+@AcraCore(buildConfigClass = BuildConfig.class)
+@AcraMailSender(mailTo = "watch.my.grow@gmail.com")
+@AcraDialog(resText = R.string.dialog_text,
+        resCommentPrompt = R.string.dialog_comment)
+public class PlantTracker extends Application {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        // The following line triggers the initialization of ACRA
+        ACRA.init(this);
+    }
+}
